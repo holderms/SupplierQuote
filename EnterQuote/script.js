39b2366b72e69f8846fe06c8316932dc7d72ce12
holderms <32437358+holderms@users.noreply.github.com>
2017-11-14 23:11:05 -0800

    enter1

---
+++ EnterQuote/script.js
@@ -0,0 +1,11 @@
+function getRandom{
+  function getRandomInt(1000, 2500) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+document.getElementById("randomNo");
+randomNo = getRandom();
+
+document.append(randomNo);
