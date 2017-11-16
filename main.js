fd333dd1f168cdd5141427c9fe0986336361867c
holderms <32437358+holderms@users.noreply.github.com>
2017-11-15 21:14:43 -0800

    main2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-
-var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+// //var express = require('express');
+//
+// var app = express();
+// var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
