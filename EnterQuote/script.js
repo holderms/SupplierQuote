c3cdc48a7ac9fa92fb1e9f2008e8be1cef1e2980
holderms <32437358+holderms@users.noreply.github.com>
2017-11-15 19:56:39 -0800

    v2

--- EnterQuote/script.js
+++ EnterQuote/script.js
@@ -1,13 +1,17 @@
-function getRandom{
-  function getRandomInt(1000, 2500) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
+var randomNo = document.getElementById("randomNo");
 
-document.getElementById("randomNo");
-randomNo = getRandom();
+randomNo = function getRandomInt(1000, 2500) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
 
 document.append(randomNo);
 
 //Reference https://css-tricks.com/generate-a-random-number/ to remind me about <div>
+var quoteNo = document.getElementById("quoteNo");
+var fs = require("fs");
+var content = fs.readFileSync("quoteCount.json");
+console.log(content[0]);
+quoteNo = content[0];
+document.append(quoteNo);
