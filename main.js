7c96100cc10ba2326d45572fc222f5cba8159a2d
holderms <32437358+holderms@users.noreply.github.com>
2017-11-15 21:53:50 -0800

    sldjf

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ var password = document.getElementById("password");
 
 if(username === "employeeTest" && password === "testpassword"){
   //https://stackoverflow.com/questions/503093/how-to-redirect-to-another-webpage
-  window.location.replace("EnterQuote.index");
+  console.log("Received")
+  window.location.replace("EnterQuote/index.html");
+} else {
+  console.log("Wrong username/password combination. Please try again.")
 }
