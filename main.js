551249de139c6ef4c14bb292788c4ccb2710f010
holderms <32437358+holderms@users.noreply.github.com>
2017-11-16 18:07:49 -0800

    Revert "get"
    This reverts commit eec3b9c2b785c91cf982d6e3b3f6c3e14feaf0e0.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var username = getElementById("username");
 var password = getElementById("password");
 
@@ -9,3 +10,96 @@ if(username === "employeeTest" && password === "testpassword"){
   console.log("Wrong username/password combination. Please try again.")
   console.log("Username: " + username + " Password: " + password)
 }
+=======
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+//Post info
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 14768);
+
+//From lecture; Send data to get or postRes view & display as a list
+app.get('/index.html',function(req,res){
+  var params = [];
+  for (var b in req.query){
+    params.push({'name':b,'value':req.body[b]})
+  }
+  console.log("in get");
+  var login = false;
+  if(params[0].name=="username" && params[0].value=="employeeTest"){
+    if(params[1].name=="password" && params[1].value=="testpassword"){
+      login = true;
+      res.sendFile("EnterQuote/index.html");
+    }
+  } else {
+    res.render('loginError', context);
+  }
+ });
+
+
+//Pass in body via ARC
+app.post('SupplierQuote/index.html',function(req,res){
+  // var qParams = [];
+  // for (var p in req.query){
+  //   qParams.push({'name':p,'value':req.query[p]})
+  // }
+  // var context = {};
+  // context.query = qParams;
+  console.log("in post");
+
+  var login = false;
+  if(params[0].name=="username" && params[0].value=="employeeTest"){
+    if(params[1].name=="password" && params[1].value=="testpassword"){
+      login = true;
+      res.sendFile("EnterQuote/index.html");
+    }
+  } else {
+    res.render('loginError', context);
+  }
+
+
+  // var bParams = [];
+  // for (var b in req.body){
+  //   bParams.push({'name':b,'value':req.body[b]})
+  // }
+  // var login = false;
+  // if(bParams[0].name=="username" && bParams[0].value=="employeeTest"){
+  //   if(bParams[1].name=="password" && bParams[1].value=="testpassword"){
+  //     login = true;
+  //     res.sendFile("EnterQuote/index.html");
+  //   }
+  // } else {
+  //   res.render('loginError', context);
+  // }
+  // console.log(bParams);
+  // console.log(req.body);
+ });
+
+
+
+
+
+//From lecture
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+>>>>>>> parent of eec3b9c... get
